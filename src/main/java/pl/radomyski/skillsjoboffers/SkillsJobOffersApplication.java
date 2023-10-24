5b006876f635e5af36b56dea8b6e8f1a44bb6227
Jakub Radomyski <zakubrad@gmail.com>
2023-10-24 23:44:45 +0200

    Initial

---
+++ src/main/java/pl/radomyski/skillsjoboffers/SkillsJobOffersApplication.java
@@ -0,0 +1,13 @@
+package pl.radomyski.skillsjoboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsJobOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsJobOffersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/radomyski/skillsjoboffers/SkillsJobOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.radomyski.skillsjoboffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsJobOffersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
