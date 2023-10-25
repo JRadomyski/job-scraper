68f7643da39dce25e901d23e923bf492ae8ca4da
Jakub Radomyski <zakubrad@gmail.com>
2023-10-25 18:56:42 +0200

    Making JobController a RestController

--- src/main/java/pl/radomyski/skillsjoboffers/JobController.java
+++ src/main/java/pl/radomyski/skillsjoboffers/JobController.java
@@ -2,35 +2,36 @@ package pl.radomyski.skillsjoboffers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController("/api")
 public class JobController {
 
     ObjectMapper objectMapper = new ObjectMapper();
+    Map<String, Integer> skillCount = new HashMap<>();
 
-    @EventListener(ApplicationReadyEvent.class)
-    public String get() throws IOException {
+    @GetMapping("/api")
+    public Map<String, Integer> get() throws IOException {
 
         String jobOffers = Files.readString(Path.of("src/main/resources/jobs.json"));
         JsonNode jsonParent = objectMapper.readTree(jobOffers);
-
         List<JsonNode> jsonNodes = jsonParent.findValues("requiredSkills");
 
-        System.out.print(jsonNodes.size());
+        for (JsonNode jsonNode : jsonNodes) {
+            for (JsonNode node : jsonNode) {
+                String skillName = node.asText();
+                skillCount.put(skillName, skillCount.getOrDefault(skillName, 0) + 1);
+            }
+        }
 
-        System.out.print(jsonNodes);
-
-
-        return null;
+        return skillCount;
     }
-
-
 }
